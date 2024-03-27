540402017545cb3de0a3b0720ef061a20e493b07
Luchianova Anna <anyalukyanova1@gmail.com>
2024-03-28 00:39:36 +0200

    Added homework

---
+++ src/Homework/Square.java
@@ -0,0 +1,14 @@
+package Homework;
+public class Square {
+    public static void main(String[] args) {
+        int area;
+        int side;
+
+        side = 2;
+        area = side * side;
+        System.out.println("Площадь квадрата равна:" + area );
+    }
+}
+
+
+
